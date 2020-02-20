d34ac5afb4615f0b58697aae825824d69cf354e6
n.kruchinkin <n.kruchinkin@petrovich.ru>
2020-02-20 20:54:22 +0300

    Styles

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,2 +1,3 @@
 
-$(function(){console.log('init');});
+function initMap(){var uluru={lat:55.709759,lng:37.597026};var map=new google.maps.Map(document.getElementById('map'),{zoom:16,center:uluru,disableDefaultUI:true});var icon={url:'/img/marker.png',scaledSize:new google.maps.Size(50,50),origin:new google.maps.Point(0,0),anchor:new google.maps.Point(0,0)};var marker=new google.maps.Marker({position:uluru,map:map,icon:icon});}
+$(function(){console.log('init');initMap();});
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,25 @@
-$(function (){
-  console.log('init');
+function initMap() {
+    var uluru = {lat: 55.709759, lng: 37.597026};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: uluru,
+        disableDefaultUI: true
+    });
+    var icon = {
+        url: '/img/marker.png',
+        scaledSize: new google.maps.Size(50, 50), // scaled size
+        origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        icon: icon
+    });
+}
+
+$(function () {
+    console.log('init');
+    initMap();
 });
